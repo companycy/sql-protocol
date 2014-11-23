0da91854b741d3980acd32a88b95219628db47fb
bjcheny <bjcheny@bjchenys-MacBookPro.local>
2014-11-23 23:33:37 +0800

    1. forward packets between driver and ms sql server 2. refine code a little 3. todo:    based on tds protocol on http://www.freetds.org/tds.html#login7,    the packet starts with 0x01 contains sql query after first byte.    need more investigation.

--- proxyserver.c
+++ proxyserver.c
@@ -16,12 +16,17 @@ const int mssql_port = 1433;
 const int pg_port = 5432;
 const int mysql_port = 3306;
 
+const char mbp_ip[] = "192.168.31.202";   // host ip
+const char local_ip[] = "192.168.31.133"; // on ubuntu vm
+const char* pg_ip = local_ip;             // on ubuntu vm
+const char* mysql_ip = local_ip;          // on ubuntu vm
+const char mssql_ip[] = "192.168.31.184"; // on win7 vm
+
 void error(char *msg) {
   perror(msg);
   exit(1);
 }
 
-// todo:
 void setopt(const int recvfd) {
   const int optval = 1;
   if (setsockopt(recvfd, SOL_SOCKET, SO_REUSEADDR,
@@ -37,33 +42,61 @@ void setopt(const int recvfd) {
     error("ERROR on setting keepalive");
 }
 
-int main(int argc, char**argv) {
-  const int recvfd = socket(AF_INET, SOCK_STREAM, 0);
-  if (recvfd < 0) {
+int get_sendfd(const char* sqlserver_ip, const int sqlserver_port) {
+  const int sendfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (sendfd < 0) {
     error("ERROR opening socket");
+  } else if (!sqlserver_ip || *sqlserver_ip == '\0') {
+    error("ERROR on sql server ip");
   } else {
-    // printf("new socket %d\n", recvfd);
-    setopt(recvfd);
+    // printf("new socket %d\n", sendfd);
+    setopt(sendfd);
   }
 
-  const int port = pg_port;
+  struct sockaddr_in sendaddr;
+  bzero(&sendaddr, sizeof(sendaddr));
+  sendaddr.sin_family = AF_INET;
+  sendaddr.sin_addr.s_addr = inet_addr(sqlserver_ip);
+  sendaddr.sin_port = htons(sqlserver_port);
+  if (connect(sendfd, (struct sockaddr *) &sendaddr,sizeof(sendaddr)) < 0) 
+    error("ERROR connecting");
+  else
+    printf("connected to backend sql server from port: %d\n", sqlserver_port);
+
+  return sendfd;
+}
+
+int main(int argc, char**argv) {
+  // config here
+  const int fe_port = mssql_port;  // accept connection from driver
+  const char* be_ip = mssql_ip;    // sql server ip
+  const int be_port = mssql_port;
+
+  const int socketfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (socketfd < 0) {
+    error("ERROR opening socket");
+  } else {
+    // printf("new socket %d\n", socketfd);
+    setopt(socketfd);
+  }
+
   struct sockaddr_in servaddr;
   bzero(&servaddr, sizeof(servaddr));
-  servaddr.sin_family = AF_INET;
   servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-  servaddr.sin_port = htons(port);
-  if (bind(recvfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
+  servaddr.sin_port = htons(fe_port);
+  servaddr.sin_family = AF_INET;
+  if (bind(socketfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
     error("ERROR on binding");
 
   const int maxconn = 1024;
-  if (listen(recvfd, maxconn) < 0)
+  if (listen(socketfd, maxconn) < 0)
     error("ERROR on listen");
 
   struct sockaddr_in cliaddr;
   socklen_t clilen = sizeof(cliaddr);
   // for(;;)
   {
-    const int connfd = accept(recvfd, (struct sockaddr *)&cliaddr, &clilen); // accept is blocking
+    const int connfd = accept(socketfd, (struct sockaddr *)&cliaddr, &clilen); // accept is blocking
     if (connfd < 0) {
       error("ERROR on accept");
     } else {
@@ -75,30 +108,12 @@ int main(int argc, char**argv) {
     if (hostp == NULL)
       error("ERROR on gethostbyaddr");
 
-    char* hostaddrp = inet_ntoa(cliaddr.sin_addr);
+    const char* hostaddrp = inet_ntoa(cliaddr.sin_addr);
     if (hostaddrp == NULL)
       error("ERROR on inet_ntoa\n");
-    printf("server established connection with %s (%s) from port: %d\n", hostp->h_name, hostaddrp, port);
+    printf("server established connection with %s (%s) from port: %d\n", hostp->h_name, hostaddrp, fe_port);
 
-    // open a new socket to connect ms sql server
-    const int sendfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sendfd < 0) {
-      error("ERROR opening socket");
-    } else {
-      // printf("new socket %d\n", sendfd);
-      setopt(sendfd);
-    }
-
-    struct sockaddr_in sendaddr;
-    bzero(&sendaddr, sizeof(sendaddr));
-    sendaddr.sin_family = AF_INET;
-    sendaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    const int actport = port + 1;
-    sendaddr.sin_port = htons(actport);    // port to connect actual sql server
-    if (connect(sendfd, (struct sockaddr *) &sendaddr,sizeof(sendaddr)) < 0) 
-      error("ERROR connecting");
-    else
-      printf("connected to backend sql server from port: %d\n", actport);
+    const int sendfd = get_sendfd(be_ip, be_port); // new socket to connect ms sql server
 
     char buf[1024];
     bzero(buf, sizeof(buf));
@@ -106,31 +121,41 @@ int main(int argc, char**argv) {
     for (;;) {
       const int n = read(connfd, buf, sizeof(buf));
       if (n > 0) {
-        printf("server received %d bytes\n", n);
+        printf("from frontend:  %d bytes\n", n);
 
-        // for (int i = 0; i < n; ++i) {
-        //   printf("%d", *(buf + i));
-        // }
-        // printf("\n");
+        for (int i = 0; i < n; ++i) {
+          printf("%c", *(buf + i));
+        }
+        printf("\n");
+
+
+        if (buf[0] == 0x01) {  // from tds protocol v7, it's a query packet
+          printf("\n");
+          printf("\n");
+          // char query[1024];
+          // bzero(query, sizeof(query));
+          // strncpy(query, (const char*)(buf + 1), n - 1);
+          printf("one more packet, and first byte: %d, left bytes: %s\n", buf[0], buf + 1);
+        }
 
         const int m = write(sendfd, buf, n);
         if (m < 0)
           error("ERROR writing to socket");
         else
-          printf("write %d bytes, total %d bytes\n", m, n);
+          printf("write to backend %d bytes, total %d bytes\n", m, n);
 
         bzero(buf, sizeof(buf));
         const int j = read(sendfd, buf, sizeof(buf));
         if (j < 0)
           error("ERROR reading from socket");
         else
-          printf("read from backend server bytes:  %d\n", j);
+          printf("read from backend bytes:  %d\n", j);
 
         const int k = write(connfd, buf, j);
         if (k < 0)
           error("ERROR writing to socket");
         else
-          printf("write back to front end bytes: %d\n", k);
+          printf("write to frontend bytes: %d\n", k);
 
       }
 
